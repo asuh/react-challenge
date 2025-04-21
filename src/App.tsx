d6b4e246878ccabad0f554025c807a436a55eb28
Micah <micah@asuh.com>
2025-04-21 19:51:12 -0400

    Add global home link to h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PokemonTable from './components/PokemonTable';
 import PokemonDetails from './components/PokemonDetails';
@@ -8,7 +9,7 @@ import { PokemonCacheProvider } from './context/PokemonCacheContext';
 const App: React.FC = () => (
   <PokemonCacheProvider>
     <Router>
-      <h1>Pokemon List</h1>
+      <h1><Link to="/">Pokemon List</Link></h1>
       <Routes>
         <Route path="/" element={<PokemonTable />} />
         <Route path="/pokemon/:name" element={<PokemonDetails />} />

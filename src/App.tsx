ee03de2ac924b5ee4d7a0328b3e05beec6df2b5a
Micah <micah@asuh.com>
2025-05-01 22:35:30 -0400

    Remove PokemonCacheContext
    When I transitioned to Tanstack Query, I deprecated my caching system

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PokemonTable from './components/PokemonTable';
 import PokemonDetails from './components/PokemonDetails';
 import './App.css';
-import { PokemonCacheProvider } from './context/PokemonCacheContext';
 
 const App: React.FC = () => (
-  <PokemonCacheProvider>
-    <Router>
-      <h1>
-        <Link to='/'>Pokemon List</Link>
-      </h1>
-      <Routes>
-        <Route path='/' element={<PokemonTable />} />
-        <Route path='/pokemon/:name' element={<PokemonDetails />} />
-      </Routes>
-    </Router>
-  </PokemonCacheProvider>
+  <Router>
+    <h1>
+      <Link to='/'>Pokemon List</Link>
+    </h1>
+    <Routes>
+      <Route path='/' element={<PokemonTable />} />
+      <Route path='/pokemon/:name' element={<PokemonDetails />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
